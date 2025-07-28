01ec1a038bb60f745ae6e6ad129b69fbe7645c2e
khaledssbd <khaledssbd@gmail.com>
2025-07-28 16:02:21 +0600

    update: in Navbar added  starts by Search icon

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -247,11 +247,31 @@ const Navbar = () => {
               />
             </Link>
             <div className="relative hidden lg:block">
-              <Search className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
+              <div className="absolute left-3 top-1/2 -translate-y-1/2 text-orange-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-sparkles w-5 h-5"
+                >
+                  <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
+                  <path d="M5 3v4" />
+                  <path d="M19 17v4" />
+                  <path d="M3 5h4" />
+                  <path d="M17 19h4" />
+                </svg>
+              </div>
+              <Search className="absolute left-10 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
               <input
                 type="text"
                 placeholder="স্কিলস কোর্স, কিংবা স্কুল প্রোগ্রাম সার্চ করুন..."
-                className="pl-12 pr-4 py-3 w-[350px] border border-gray-300 rounded-full focus:outline-none focus:ring-1 focus:ring-gray-400 text-sm"
+                className="pl-16 pr-4 py-3 w-[350px] border border-gray-300 rounded-full focus:outline-none focus:ring-1 focus:ring-gray-400 text-sm"
               />
             </div>
           </div>
