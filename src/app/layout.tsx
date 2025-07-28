a19d00451c11d0cb9164b6c3b2e7bdfa3e4302ac
khaledssbd <khaledssbd@gmail.com>
2025-07-28 13:55:00 +0600

    update: font inter added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,13 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/shared/Navbar';
 import Footer from '@/components/shared/Footer';
 import SocialSection from '@/components/Products/SocialSection';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const inter = Inter({
+  variable: '--font-inter-regular',
   subsets: ['latin'],
 });
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
-export const viewport = {
-  themeColor: '#0ea5e9', // for Address bar // Tailwind sky-500
-};
 
 export default function RootLayout({
   children,
@@ -25,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.className} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
@@ -36,4 +25,3 @@ export default function RootLayout({
     </html>
   );
 }
-
